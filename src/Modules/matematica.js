bbf93235731559066a6489a4265002fb37e9b66f
roni <47867214@ort>
2024-04-15 08:38:08 -0300

    arreglos

--- src/Modules/matematica.js
+++ src/Modules/matematica.js
@@ -8,6 +8,10 @@ const multiplicar = (x, y) =>
  return x*y;
 }
 
+const dividir = (x,y) => {return x/y;}
+
+const restar = (x,y)=>{return x-y;}
+
 const strings = ["dos", "cuatro","ocho","diez"];
 
-export{Pi, suma, multiplicar, strings};
+export{Pi, suma, restar, multiplicar, dividir, strings};
